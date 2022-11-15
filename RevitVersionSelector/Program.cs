9b6137be8a17f8aa6d76cf33b2cf0d774fee648d
dosymep <dosymep@gmail.com>
2022-11-15 12:00:01 +0300

    added message when not found revit

--- RevitVersionSelector/Program.cs
+++ RevitVersionSelector/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Linq;
+using System.Windows;
 
 using dosymep.Revit.FileInfo;
 
@@ -11,16 +12,23 @@ namespace RevitVersionSelector {
         public static void Main(string[] args) {
             if(args.Length == 1) {
                 string revitFilePath = args[0];
-                var revitFileInfo = new RevitFileInfo(revitFilePath);
+                try {
+                    var revitFileInfo = new RevitFileInfo(revitFilePath);
+                    string format = revitFileInfo.BasicFileInfo.AppInfo.Format;
+                    RevitProduct revitProduct = RevitProduct.GetInstalledProducts()
+                        .FirstOrDefault(item => item.RevitVersion.Equals(format));
 
-                string format = revitFileInfo.BasicFileInfo.AppInfo.Format;
-                RevitProduct revitProduct = RevitProduct.GetInstalledProducts()
-                    .FirstOrDefault(item => item.RevitVersion.Equals(format));
-
-                if(revitProduct == null) {
+                    if(revitProduct == null) {
+                        var result = MessageBox.Show(StringResources.MessageBoxContent,
+                            StringResources.MessageBoxTitle, MessageBoxButton.YesNo, MessageBoxImage.Warning);
+                        if(result == MessageBoxResult.Yes) {
+                            Process.Start("explorer.exe", revitFilePath);
+                        }
+                    } else {
+                        Process.Start(revitProduct.ApplicationFilePath, revitFilePath);
+                    }
+                } catch {
                     Process.Start("explorer.exe", revitFilePath);
-                } else {
-                    Process.Start(revitProduct.ApplicationFilePath, revitFilePath);
                 }
             }
         }
